c735f02d0a44c480bdd256833f3a5f926eb9ad44
Michal Kucera <m-kucera@outlook.com>
2023-11-28 16:40:44 +0100

    podklady

---
+++ Procvicovani/script.js
@@ -0,0 +1,72 @@
+/*------------------------------- TASK 01 -----------------------------------
+Proměnná medalTable obsahuje zisky medailí států na olympiádě 2020. Vypiš
+název státu, který získal nejvíc medailí.*/
+
+medalTable = {
+    Kenya: 10,
+    Australia: 46,
+    Iran: 7,
+    Ecuador: 3,
+    Portugal: 4,
+    Kazakhstan: 8,
+}
+
+
+/*------------------------------- TASK 02 -----------------------------------
+Proměnná medalTable tentokrát obsahuje zisky jednotlivých druhů medailí států na olympiádě 2020. */
+
+medalTable = {
+    "Cuba": { "gold": 7, "silver": 3, "bronze": 5 },
+    "Spain": { "gold": 3, "silver": 8, "bronze": 6 },
+    "Uganda": { "gold": 2, "silver": 1, "bronze": 1 },
+    "Bahamas": { "gold": 2, "silver": 0, "bronze": 0 },
+    "Ukraine": { "gold": 1, "silver": 6, "bronze": 12 },
+    "San Marino": { "gold": 0, "silver": 1, "bronze": 2 },
+}
+
+// Vypiš název státu, který získal nejvíc bronzových medailí.
+
+
+
+/*------------------------------- TASK 03 -----------------------------------
+Napiš funkci map_multiply, která vezme jako parametry pole čísel a číslo a vrátí pole čísel vynásobený číslem z druhého parametru. */
+
+
+
+
+
+/*------------------------------- TASK 04 -----------------------------------
+Pomocí cyklu vypište všechna hesla, která mají více než 8 znaků*/
+
+hesla = [
+    "xyz101",
+    "punťa",
+    "razor-blade",
+    "1234",
+    "12011986",
+    "martin111",
+    "trhnisi",
+    "hokuspokus",
+    "jeník15",
+    "kristus-te-spasi",
+    "beruška",
+    "strčprstskrzkrk",
+]
+
+
+/*------------------------------- TASK 05 -----------------------------------
+Vymysli algoritmus, který po uživateli bude chtít zadat uživatelské heslo. Pokud toto heslo není správné, uživateli se žádost o zadaní hesla zobrazí znovu.*/
+
+
+
+
+/*------------------------------- TASK 06 -----------------------------------
+Je připravené heslo v databázi. Postupně se zeptej uživatele na zadání 2., 5. a 7. znaku, pokud zadá všechny znaky správně, vypiš mu, že je přihlášený. V opačném případě zobraz hlášu "Špatné zadání!".*/
+
+const heslo = "Czechitas12"
+
+
+
+/*------------------------------- TASK 07 -----------------------------------
+Vytvoř algoritmus na výpočet obecného faktoriálu. Nevíš, co je to faktoriál? Mrkni sem - https://cs.wikipedia.org/wiki/Faktori%C3%A1l. Například !5 = 120*/
+
